d72bab7b715fe88e315fd696059d01edb3629192
aerayalkan <ahmeterayalkan@gmail.com>
2024-07-14 00:46:16 +0300

    Preparations of files

---
+++ src/main/java/com/erayalkan/Personal_and_Inventory_Management_App/PersonalAndInventoryManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.erayalkan.Personal_and_Inventory_Management_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalAndInventoryManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalAndInventoryManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erayalkan/Personal_and_Inventory_Management_App/PersonalAndInventoryManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erayalkan.Personal_and_Inventory_Management_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalAndInventoryManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
