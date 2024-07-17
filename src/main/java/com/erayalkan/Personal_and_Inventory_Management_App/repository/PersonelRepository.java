d4d0117e43e712d05c4977031bab56782e76b464
aerayalkan <ahmeterayalkan@gmail.com>
2024-07-17 20:33:12 +0300

    Preparations of pockets

---
+++ src/main/java/com/erayalkan/Personal_and_Inventory_Management_App/repository/PersonelRepository.java
@@ -0,0 +1,9 @@
+package com.erayalkan.Personal_and_Inventory_Management_App.repository;
+
+import com.erayalkan.Personal_and_Inventory_Management_App.model.Personal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+public interface PersonelRepository extends JpaRepository<Personal, Long> {
+}
